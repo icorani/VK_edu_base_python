4bd2041c36d3aa8e55a503f471e0dc1d4711cfa3
icorani <icorani@yandex.ru>
2024-09-28 04:28:11 +0300

    feat: reformat file

--- mod4/lect3-1.py
+++ mod4/lect3-1.py
@@ -5,8 +5,8 @@ days, seconds = int(input()), int(input())
 
 def shift_time(days: int, seconds: int):
     return tuple(map(int, datetime.datetime.strftime(datetime.timedelta(days=days, seconds=seconds)
-                                      + datetime.datetime.fromisoformat('2023-01-01 12:30:00'),
-                                      '%d %S').split()))
+                                                     + datetime.datetime.fromisoformat('2023-01-01 12:30:00'),
+                                                     '%d %S').split()))
 
 
 print(shift_time(days, seconds))
